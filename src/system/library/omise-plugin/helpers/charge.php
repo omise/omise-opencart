f08c9f8968bfe40c2fb81d3e376659398d4f36c3
guzzilar <iam.nuttanon@gmail.com>
2017-02-16 15:06:02 +0530

    Be able to create charge with IDR, SGD currencies.

--- src/system/library/omise-plugin/helpers/charge.php
+++ src/system/library/omise-plugin/helpers/charge.php
@@ -3,22 +3,22 @@ if (! class_exists('OmisePluginHelperCharge')) {
     class OmisePluginHelperCharge
     {
         /**
-         * @param string  $currency
-         * @param integer $amount
+         * Format a Magento's amount to be a small-unit that Omise's API requires.
+         * Note, no specific format for JPY currency.
+         *
+         * @param  string  $currency
+         * @param  integer $amount
+         *
          * @return string
          */
         public static function amount($currency, $amount)
         {
             switch (strtoupper($currency)) {
                 case 'THB':
-                    // Convert to satang unit
+                case 'IDR':
+                case 'SGD':
+                    // Convert to a small unit
                     $amount = $amount * 100;
-                    break;
-
-                case 'JPY':
-                    break;
-
-                default:
                     break;
             }
 
