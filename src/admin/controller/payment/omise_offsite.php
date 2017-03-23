89c7ea399a05a50e7920f69d5bd9d930fa621888
iwat <chaiwat@omise.co>
2017-03-23 10:21:55 +0700

    Add new payment method 'Omise Payment Gateway - Internet Banking'

---
+++ src/admin/controller/payment/omise_offsite.php
@@ -0,0 +1,218 @@
+<?php
+class ControllerPaymentOmiseOffsite extends Controller {
+    /**
+     * @var array
+     */
+    private $error = array();
+
+    /**
+     * @return string
+     */
+    private function flashSuccessMessages() {
+        if (isset($this->session->data['success'])) {
+            $msg = $this->session->data['success'];
+            unset($this->session->data['success']);
+
+            return $msg;
+        }
+
+        return "";
+    }
+
+    /**
+     * @return string
+     */
+    private function flashErrorMessages() {
+        if (isset($this->session->data['error'])) {
+            $msg = $this->session->data['error'];
+            unset($this->session->data['error']);
+
+            return $msg;
+        }
+
+        return "";
+    }
+
+    /**
+     * Set page breadcrumb
+     * @return array
+     */
+    private function setBreadcrumb($current = null) {
+        $this->load->language('payment/omise_offsite');
+
+        // Set Breadcrumbs.
+        $breadcrumbs = array();
+
+        $breadcrumbs[] = array(
+            'text'      => $this->language->get('text_home'),
+            'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+            'separator' => false
+        );
+
+        $breadcrumbs[] = array(
+            'text'      => $this->language->get('text_payment'),
+            'href'      => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'),
+            'separator' => ' :: '
+        );
+
+        $breadcrumbs[] = array(
+            'text'      => $this->language->get('heading_title'),
+            'href'      => $this->url->link('payment/omise_offsite', 'token=' . $this->session->data['token'], 'SSL'),
+            'separator' => ' :: '
+        );
+
+        if (! is_null($current))
+            $breadcrumbs[] = $current;
+
+        return $breadcrumbs;
+    }
+ 
+    /**
+     * @return array
+     */
+    private function pageDataSettingTab() {
+        return array(
+            'omise_offsite_status'        => $this->config->get('omise_offsite_status'),
+            'omise_offsite_payment_title' => $this->config->get('omise_offsite_payment_title')
+        );
+    }
+
+    /**
+     * @return array
+     */
+    private function pageTranslation() {
+        $this->load->language('payment/omise_offsite');
+
+        return array(
+            'heading_title'                     => $this->language->get('heading_title'),
+            'label_setting_module_config'       => $this->language->get('label_setting_module_config'),
+            'label_setting_module_status'       => $this->language->get('label_setting_module_status'),
+            'label_omise_offsite_payment_title' => $this->language->get('label_omise_offsite_payment_title'),
+            'text_enabled'                      => $this->language->get('text_enabled'),
+            'text_disabled'                     => $this->language->get('text_disabled'),
+            'button_save'                       => $this->language->get('button_save'),
+            'button_cancel'                     => $this->language->get('button_cancel'),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function searchErrorTranslation($clue) {
+        $this->load->language('payment/omise_offsite');
+
+        $translate_code = 'error_' . str_replace(' ', '_', strtolower($clue));
+        $translate_msg  = $this->language->get($translate_code);
+
+        if ($translate_code !== $translate_msg)
+            return $translate_msg;
+
+        return $clue;
+    }
+
+    /**
+     * @return void
+     */
+    private function redirectTo($destination) {
+        switch ($destination) {
+            case 'omise_dashboard':
+                $this->response->redirect($this->url->link('payment/omise_offsite', 'token=' . $this->session->data['token'], 'SSL'));
+                break;
+
+            default:
+                $this->response->redirect($this->url->link('payment/omise_offsite', 'token=' . $this->session->data['token'], 'SSL'));
+                break;
+        }
+    }
+
+    /**
+     * This method will fire when user click `install` button from `extension/payment` page
+     * It will call `model/payment/omise_offsite.php` file and run `install` method for installl stuff
+     * that necessary to use in Omise Payment Gateway Internet Banking module
+     * @return void
+     */
+    public function install() {
+        $this->load->model('payment/omise_offsite');
+
+        try {
+            // Install the extension
+            if (! $this->model_payment_omise_offsite->install())
+                throw new Exception('', 1);
+        } catch (Exception $e) {
+            // Uninstall
+            $this->load->controller('extension/payment/uninstall');
+        }
+    }
+
+    /**
+     * This method will fire when user click `Uninstall` button from `extension/payment` page
+     * Uninstall everything that related with Omise Payment Gateway module.
+     * @return void
+     */
+    public function uninstall() {
+
+    }
+
+    /**
+     * (GET) Page, route=payment/omise_offsite
+     */
+    public function index() {
+        // POST Request handle
+        if (($this->request->server['REQUEST_METHOD'] == 'POST'))
+            $this->updateConfig();
+
+        $this->load->language('payment/omise_offsite');
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        // Manipulate page's data
+        $data = array_merge(
+            $this->pageDataSettingTab(),
+            $this->pageTranslation(),
+            array(
+                'success'            => $this->flashSuccessMessages(),
+                'error_warning'      => $this->flashErrorMessages(),
+                'action'             => $this->url->link('payment/omise_offsite', 'token=' . $this->session->data['token'], 'SSL'),
+                'cancel'             => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'),
+            )
+        );
+
+        // Page templates
+        $data = array_merge($data, array(
+            'header'      => $this->load->controller('common/header'),
+            'breadcrumbs' => $this->setBreadcrumb(null),
+            'column_left' => $this->load->controller('common/column_left'),
+            'footer'      => $this->load->controller('common/footer')
+        ));
+
+        $this->response->setOutput($this->load->view('payment/omise_offsite.tpl', $data));
+    }
+
+    /**
+     * (POST)
+     * @return void
+     */
+    public function updateConfig() {
+        $this->load->model('setting/setting');
+        $this->load->language('payment/omise_offsite');
+
+        try {
+            // Allowed only POST method
+            if ($this->request->server['REQUEST_METHOD'] !== 'POST')
+                throw new Exception($this->language->get('error_allowed_only_post_method'), 1);
+
+            $update = $this->request->post;
+            if (! empty($update)) {
+                if ($update['omise_offsite_status'] == 1 && $this->config->get('omise_status') != 1) {
+                    throw new Exception($this->language->get('error_need_omise_extension'));
+                }
+                // Update
+                $this->model_setting_setting->editSetting('omise_offsite', $update);
+                $this->session->data['success'] = $this->language->get('text_session_save');
+            }
+        } catch (Exception $e) {
+            $this->session->data['error'] = $this->searchErrorTranslation($e->getMessage());
+        }
+
+        $this->redirectTo('omise_offsite_setting');
+    }
+}
---
+++ src/admin/language/japanese/payment/omise_offsite.php
@@ -0,0 +1,36 @@
+< ?php
+// Module title
+$_['heading_title']                                            = 'Omise Payment Gateway - Internet Banking';
+$_['text_omise_offsite']                                       = '<a href="https://www.omise.co/ja" target="_blank" style="border: 1px solid #EEEEEE; padding: 2px; width: 94px; max-height:25px; display: inline-block;" alt="Omise Payment Gateway"" title="Omise Payment Gateway"><img src="view/image/payment/omise-payment.png" alt="Omise Payment Gateway"" title="Omise Payment Gateway" style="max-height:18px;" /></a>';
+
+
+// Labels
+$_['label_setting_module_config']                              = 'モジュール設定';
+$_['label_setting_module_status']                              = 'モジュール状況';
+
+
+// Omise's labels
+$_['label_omise_offsite_payment_title']                        = '決済方法';
+
+
+// Breadcrumb menu.
+$_['text_home']                                                = 'ホーム';
+$_['text_payment']                                             = '決済';
+
+
+// Messages
+$_['text_enabled']                                             = '有効化';
+$_['text_disabled']                                            = '無効化';
+$_['text_session_save']                                        = '保存されました';
+
+
+// Action buttons
+$_['button_save']                                              = '保存';
+$_['button_cancel']                                            = 'キャンセル';
+
+
+// Errors
+$_['error_need_omise_extension']                               = 'Please install and enable <strong>Omise Payment Gateway</strong> extension before enabling <strong>Omise Payment Gateway - Internet Banking</strong> (check \'Setting\' tab).';
+
+
+?>
