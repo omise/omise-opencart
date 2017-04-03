ace5cba3f3cc465f8ce5be56e7d7a1de3b68ab10
iwat <chaiwat@omise.co>
2017-04-03 08:33:52 +0700

    Fix broken Aop class loader
    The original version tries to load class file with back-slash (\)
    prefix, this commit fixes that by using `ltrim` function on the
    overriding class of `ComposerLoader`.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,26 @@ if (!defined('DB_PREFIX')) {
 
 require_once(__DIR__.'/../vendor/autoload.php');
 
+$loaders = spl_autoload_functions();
+
 $kernel = \AspectMock\Kernel::getInstance();
 $kernel->init([
         'debug' => true,
         'includePaths' => [__DIR__.'/../src/system']
     ]);
+unset($kernel);
+
+class PatchedComposerLocator extends \Go\ParserReflection\Locator\ComposerLocator
+{
+     public function locateClass($className)
+     {
+        $className = ltrim($className, '\\');
+        return parent::locateClass($className);
+     }
+}
+
+\Go\ParserReflection\ReflectionEngine::init(new PatchedComposerLocator($loaders[0][0]));
+unset($loaders);
 
 require_once(__DIR__.'/mock/controller.php');
 require_once(__DIR__.'/mock/mock_registry.php');
