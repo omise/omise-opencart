c00af1459e54f3d078c42b9271dba0c6f217df5a
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-04-04 12:02:49 +0700

    Format PHP code, add missing curly braces

--- src/catalog/controller/payment/omise.php
+++ src/catalog/controller/payment/omise.php
@@ -1,12 +1,14 @@
 <?php
 
 // Define 'OMISE_USER_AGENT_SUFFIX'
-if(!defined('OMISE_USER_AGENT_SUFFIX') && defined('VERSION'))
+if (!defined('OMISE_USER_AGENT_SUFFIX') && defined('VERSION')) {
     define('OMISE_USER_AGENT_SUFFIX', 'OmiseOpenCart/1.3 OpenCart/' . VERSION);
+}
 
 // Define 'OMISE_API_VERSION'
-if(!defined('OMISE_API_VERSION'))
+if (!defined('OMISE_API_VERSION')) {
     define('OMISE_API_VERSION', '2014-07-27');
+}
 
 class ControllerPaymentOmise extends Controller {
     public function checkoutCallback() {
