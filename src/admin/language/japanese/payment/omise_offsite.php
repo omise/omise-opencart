788dbe4e4e0284208a993b275b86778487b1dec3
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-04-19 11:35:56 +0700

    Correct the PHP open tag

--- src/admin/language/japanese/payment/omise_offsite.php
+++ src/admin/language/japanese/payment/omise_offsite.php
@@ -1,4 +1,4 @@
-< ?php
+<?php
 // Module title
 $_['heading_title']                                            = 'Omise Payment Gateway - Internet Banking';
 $_['text_omise_offsite']                                       = '<a href="https://www.omise.co/ja" target="_blank" style="border: 1px solid #EEEEEE; padding: 2px; width: 94px; max-height:25px; display: inline-block;" alt="Omise Payment Gateway"" title="Omise Payment Gateway"><img src="view/image/payment/omise-payment.png" alt="Omise Payment Gateway"" title="Omise Payment Gateway" style="max-height:18px;" /></a>';
